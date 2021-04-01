ee08a5dc8431cec8833e69c03b9768ae9b3aec33
DORU? IUGA <iuga.dorut165271@student.ubbcluj.ro>
2021-04-01 13:11:37 +0300

    updated our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
+import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage.component';
+import {Switch ,  Route} from 'react-router-dom';
+const HatsPage =() => (
+  <div>
+   <h1> HATS PAGE</h1> 
+  </div>
+)
 
 function App() {
-  return <div>
-    <Homepage />
-  </div>
+  return (
+  <div>
+
+    <Switch>
+    <Route exact path = '/' component={Homepage}></Route>
+    <Route  path = '/hats' component={HatsPage}></Route>
+      
+    </Switch>
+    </div>
+  
+);
   
 }
 
 export default App;
+ 
