65f4ef23cac5f23c1cfd6d37f5fb1a1e45512512
DORU? IUGA <iuga.dorut165271@student.ubbcluj.ro>
2021-04-04 14:06:24 +0300

    collection preview updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 import {Switch ,  Route} from 'react-router-dom';
-const HatsPage =() => (
-  <div>
-   <h1> HATS PAGE</h1> 
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
@@ -14,7 +11,7 @@ function App() {
 
     <Switch>
     <Route exact path = '/' component={Homepage}></Route>
-    <Route  path = '/hats' component={HatsPage}></Route>
+    <Route  path = '/shop' component={ShopPage}></Route>
       
     </Switch>
     </div>
